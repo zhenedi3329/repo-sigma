2e1f5286d57335b7b88873da596acd398cc46d90
zhenedi3329 <146415259+zhenedi3329@users.noreply.github.com>
2023-11-03 09:46:03 +0200

    Add files via upload

---
+++ hyguygjg.java
@@ -0,0 +1,12 @@
+public class hyguygjg {
+    public static void main(String[] args) {
+        String[] stringArray = new String[100];
+
+        for (int i = 0; i < 100; i++) {
+            stringArray[i] = "Елемент " + i;
+        }
+            for (int i = 0; i < 100; i++) {
+                System.out.println(stringArray[i]);
+            }
+    }
+}
